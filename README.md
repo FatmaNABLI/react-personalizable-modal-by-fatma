7560c37db1e4f17d3ece43ff79d02d86d205a7ae
Fatma NABLI <nabli.isigk@gmail.com>
2023-09-11 13:21:42 +0200

    Adding props for modal

--- README.md
+++ README.md
@@ -1,43 +1,45 @@
+
 # Getting Started
 
-  
-![Modal par défaut](C:%5CUsers%5C33750%5CDesktop%5Cprojet-14-HRnet%5Cmodal_screenshot)
 Ce package vous permet de créer une modale d'une manière simple. Vous pouvez la personnaliser en ajoutant de l'animation et vos propres couleurs.
+
 ## Les 3 types de modal
 
-<img src="src/default_modal.png" alt="default modal img" style="height: 100px; width:200px;"/>
+<img  src="src/modals.png"  alt="default/success/error Modal"/>
 
-![Default modal](src/default_modal.png)
 
-## Utilisation du composant Modal
+## Utilisation du composant Modal  
 
 ```javascript
+
 //Importer le composant Modal
-import Modal from "react-personalizable-modal-by-fatma/src/Modal"
+import  Modal  from  "react-personalizable-modal-by-fatma/src/Modal"
 
 //Dans la partie déclaration
 const [isOpen,setIsOpen]=useState(false);
-
+  
 //Dans la partie return
 <Modal  isOpen = {isOpen}  setIsOpen={setIsOpen}
 type="ERROR"
 icone={true}
-titre="Sauvegarde Employee"
-texte="Employé ajouté avec succès!"
+titre="Titre"
+texte="Texte souhaité!"
 animation = {true}  animationFrom={"LEFT"}
 />
 
-//Au clic sur le bouton 
-<button onClick={()=>{setIsOpen(true);}}>Ouvrir modal</button>
+//Au clic sur le bouton
+<button  onClick={()=>{setIsOpen(true);}}>Ouvrir modal</button>
 ```
+
 ## Les props du composant Modal
+
 | Propriété | Valeur  | Description | 
 |-- |--|--|
-|type|ERROR/SUCCESS|Sin on ne met rien , la modale aura une couleur par défaut et il n'y aura pas d'icone|
+|type|ERROR/SUCCESS|Sin on ne met rien , la modale aura une couleur par défaut et l'icone sera affichée|
 |isAnimated |true/false  |
 |animationFrom |TOP, BOTTOM, RIGHT,LEFT|La valeur par défaut est TOP|
-| title |string  |
-| text |string  |
-
-
-
+|bgColor|blue/red/#000…|Permet de changer la couleur du background du header et du bouton.|
+|containerStyle|**exemple** : containerStyle={{width:  '400px',height :'100px'}}|
+|textStyle|**exemple** : textStyle={{fontSize:  '20px',text-align:'center'}}|
+| title |string  | |
+| text |string  | |
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -33,13 +33,16 @@ function Modal(props){
             typeModal="error";
     }
     style.transition = props.animation ?   "0.3s all ease-in-out" : ""
+    Object.assign(style,props.containerStyle)
+  
     const showHideClassName = isOpen ? "modal modal--open" : `modal ${classModalStart}`;
     const showHideOverlay = isOpen ? "overlay display-block" : "overly display-none";
     let icone = true;
     if(props.hasOwnProperty("icone")){
         icone = props.icone;
     }
-
+    let textStyle = props.hasOwnProperty("textStyle")? props.textStyle : {}
+    let bgColor = props.hasOwnProperty("bgColor") ? props.bgColor : null
     return(
         <>
             {/* Close modal by outside click */}
@@ -47,26 +50,26 @@ function Modal(props){
             }} >
             </div>
             <div className={showHideClassName} style={style}>
-                <button className={`modal__close ${typeModal}`} onClick={() =>{setIsOpen(false)}}>
+                <button style = {{backgroundColor : bgColor}} className={`modal__close ${typeModal}`} onClick={() =>{setIsOpen(false)}}>
                     X
                 </button>
-                <div className={`modal__header  ${typeModal}`}>
+                <div style = {{backgroundColor : bgColor}}className={`modal__header  ${typeModal}`}>
                     <h2 className="modal__header__title">{titre}</h2>
                 </div>
                 <div className="modal__body">
                     {typeModal=="success" && icone && <svg className= "success-icone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         {/* <!--! Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
                         <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/>
-                    </svg>}
+                        </svg>}
                     
                     {typeModal=="error"&& icone && <svg className= "error-icone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         {/* <!--! Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
                         <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/>
-                    </svg>}
-                    <p>{texte}</p>
+                        </svg>}
+                    <p style={textStyle}>{texte}</p>
                 </div>
                 <div className="modal__footer">
-                    <button className={`modal__footer__button ${typeModal}`} onClick={() =>{ 
+                    <button style = {{backgroundColor : bgColor}} className={`modal__footer__button ${typeModal}`} onClick={() =>{ 
                     setIsOpen(false)}}>
                         close
                     </button>
